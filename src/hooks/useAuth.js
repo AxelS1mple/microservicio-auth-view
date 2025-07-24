55a4450587d0315f5ec2327fa315e8dc9b93dd40
Axel Nadir <axeltettw@gmail.com>
2025-07-23 22:11:01 -0300

    Nuevos cambios a la vista url del auth

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,7 +2,7 @@ import { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { AuthContext } from "../context/AuthContext";
 
-const API_URL = "https://microservicio-auth-xn3l.onrender.com/api/auth";
+const API_URL = "https://microservicio-auth-xn3l.onrender.com";
 
 export const useAuth = () => {
   const { token, login: setTokenContext, logout: logoutContext } = useContext(AuthContext);
